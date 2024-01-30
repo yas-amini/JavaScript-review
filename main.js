325bbf22adf5d6f68dcad236946a02bc89b55840
Yasamin <yasamin.amini98@gmail.com>
2024-01-30 14:44:14 +0100

    Nested Loops

---
+++ main.js
@@ -0,0 +1,9 @@
+const myArray = [6, 19, 20];
+const yourArray = [19, 81, 2];
+for (let i = 0; i < myArray.length; i++) {
+  for (let j = 0; j < yourArray.length; j++) {
+    if (myArray[i] === yourArray[j]) {
+      console.log('Both arrays have the number: ' + yourArray[j]);
+    }
+  }
+}
